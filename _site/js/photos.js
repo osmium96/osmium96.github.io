64165b971ebff5aa6eadacba407033ba0fba4981
osmium96 <34826926+osmium96@users.noreply.github.com>
2020-06-24 17:32:28 -0400

    Add files via upload

---
+++ _site/js/photos.js
@@ -0,0 +1,80 @@
+(function(html) {
+const id = document.currentScript.getAttribute('data-photo-id');
+const url = document.currentScript.getAttribute('data-photo-url');
+const target = document.currentScript.getAttribute('data-target-id');
+const container = document.querySelector(`#${target}`);
+container.innerHTML = html;
+openPhoto(id, url);
+lazyload();
+})(`
+<li class="item " id="edgewood-house" style="background-image: url('/photo/photos/tint/edgewood%20house-65ba1d.jpg')" title="edgewood house">
+  <img class="lazyload" data-src="/photo/photos/thumbnail/edgewood%20house-ccf4df.jpg" src="/photo/photos/tint/edgewood%20house-65ba1d.jpg" height="3456" width="4608" />
+  <span class="full">
+    <span style="background-image: url('/photo/photos/large/edgewood%20house-686656.jpg')"></span>
+  </span>
+  <a class="open" href="/photo/edgewood-house" data-target="edgewood-house">Open</a>
+  <a class="close" href="/photo/">Close</a>
+  <a href="/photo/laundromat" data-target="laundromat" class="next" title="Go to next photo">
+    <span>Next</span>
+  </a>
+  <!-- <ul class="meta">
+    <li>E-M10 Mark III</li>
+    <li>1/2000</li>
+    <li><span class="aperture"><em>f</em>/</span>1.8</li>
+	</ul> -->
+</li>
+<li class="item " id="laundromat" style="background-image: url('/photo/photos/tint/laundromat-65ba1d.jpg')" title="laundromat">
+  <img class="lazyload" data-src="/photo/photos/thumbnail/laundromat-ccf4df.jpg" src="/photo/photos/tint/laundromat-65ba1d.jpg" height="3456" width="4608" />
+  <span class="full">
+    <span style="background-image: url('/photo/photos/large/laundromat-686656.jpg')"></span>
+  </span>
+  <a class="open" href="/photo/laundromat" data-target="laundromat">Open</a>
+  <a class="close" href="/photo/">Close</a>
+  <a href="/photo/edgewood-house" data-target="edgewood-house" class="previous" title="Go to previous photo">
+    <span>Previous</span>
+  </a>
+  <a href="/photo/lot-by-ups" data-target="lot-by-ups" class="next" title="Go to next photo">
+    <span>Next</span>
+  </a>
+  <!-- <ul class="meta">
+    <li>E-M10 Mark III</li>
+    <li>1/640</li>
+    <li><span class="aperture"><em>f</em>/</span>4.0</li>
+	</ul> -->
+</li>
+<li class="item " id="lot-by-ups" style="background-image: url('/photo/photos/tint/lot%20by%20ups-65ba1d.jpg')" title="lot by ups">
+  <img class="lazyload" data-src="/photo/photos/thumbnail/lot%20by%20ups-ccf4df.jpg" src="/photo/photos/tint/lot%20by%20ups-65ba1d.jpg" height="3456" width="4608" />
+  <span class="full">
+    <span style="background-image: url('/photo/photos/large/lot%20by%20ups-686656.jpg')"></span>
+  </span>
+  <a class="open" href="/photo/lot-by-ups" data-target="lot-by-ups">Open</a>
+  <a class="close" href="/photo/">Close</a>
+  <a href="/photo/laundromat" data-target="laundromat" class="previous" title="Go to previous photo">
+    <span>Previous</span>
+  </a>
+  <a href="/photo/edgewood-drive" data-target="edgewood-drive" class="next" title="Go to next photo">
+    <span>Next</span>
+  </a>
+  <!-- <ul class="meta">
+    <li>E-M10 Mark III</li>
+    <li>1/2500</li>
+    <li><span class="aperture"><em>f</em>/</span>1.8</li>
+	</ul> -->
+</li>
+<li class="item " id="edgewood-drive" style="background-image: url('/photo/photos/tint/edgewood%20drive-65ba1d.jpg')" title="edgewood drive">
+  <img class="lazyload" data-src="/photo/photos/thumbnail/edgewood%20drive-ccf4df.jpg" src="/photo/photos/tint/edgewood%20drive-65ba1d.jpg" height="2819" width="3759" />
+  <span class="full">
+    <span style="background-image: url('/photo/photos/large/edgewood%20drive-686656.jpg')"></span>
+  </span>
+  <a class="open" href="/photo/edgewood-drive" data-target="edgewood-drive">Open</a>
+  <a class="close" href="/photo/">Close</a>
+  <a href="/photo/lot-by-ups" data-target="lot-by-ups" class="previous" title="Go to previous photo">
+    <span>Previous</span>
+  </a>
+  <!-- <ul class="meta">
+    <li>E-M10 Mark III</li>
+    <li>1/4000</li>
+    <li><span class="aperture"><em>f</em>/</span>1.8</li>
+	</ul> -->
+</li>
+`);
